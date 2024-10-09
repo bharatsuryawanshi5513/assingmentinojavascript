b5b2c6be10350096c3a7bf6f9fa07e7a0c683e34
bharat <bharatsuryawanshi5513@gmail.com>
2024-10-09 09:47:09 +0530

    add assingment

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-console.log(4+5);
-alert("java script")
 
-document.getElementById("output").innerHTML="bharat Suryawanshi"
+let count=0;
+document.getElementById("clickBtninc").addEventListener("click",function(){
+    count++;
+    document.getElementById("count").innerText = count;
+});
+
+document.getElementById("clickBtndec").addEventListener("click",function(){
+
+count--;
+document.getElementById("count").innerText = count;
+
+})
+
+document.getElementById("clickreset").addEventListener('click',function(){
+
+    count=0;
+    document.getElementById('count').innerText =count;
+
+})
+
+
+
+document.getElementById("clickbtndark").addEventListener('click',function(){
+
+    if
+})
